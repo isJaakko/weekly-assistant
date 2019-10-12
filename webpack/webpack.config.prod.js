f507b763f8d369cea1f6a5be82a2dca31411c8ac
isJaakko <1512803764@qq.com>
2019-10-12 17:39:28 +0800

    修改打包路径

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,6 +1,14 @@
-// const path = require('path');
+const path = require('path');
 const base = require('./webpack.config.base');
+
+const dist = '../dist';
 
 module.exports = Object.assign(base, {
   mode: 'production',
+  output: {
+    publicPath: '/weekly-assistant/',
+    path: path.resolve(__dirname, dist),
+    filename: '[name].[hash:8].js',
+    chunkFilename: 'common/[name]/[name].js',
+  },
 });
